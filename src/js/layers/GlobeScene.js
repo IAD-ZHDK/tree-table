bacac0a4e150fcc734d4488188b4718d186731fe
Luke Franzke <lukefranzke@gmail.com>
2020-02-14 18:54:48 +0100

    Added basic globe example

--- src/js/layers/GlobeScene.js
+++ src/js/layers/GlobeScene.js
@@ -1,16 +1,56 @@
 import BaseScene from './BaseScene'
-import { SphereGeometry, Color, Mesh, MeshBasicMaterial } from 'three'
+import { SphereGeometry, Mesh, MeshBasicMaterial, TextureLoader, BackSide, CylinderGeometry, Line, Vector3, BufferGeometry, LineBasicMaterial } from 'three'
 
 class GlobeScene extends BaseScene {
   setup () {
     super.setup()
+    let EarthRadius = 200
+    /* earth */
+    const EarthTexture = new TextureLoader().load('static/textures/earthMono8k.jpg')
+    let EarthGeometry = new SphereGeometry(EarthRadius, 32, 32)
+    let EarthMaterial = new MeshBasicMaterial({ map: EarthTexture })
+    this.Earth = new Mesh(EarthGeometry, EarthMaterial)
+    this.Earth.position.x = 0
+    this.add(this.Earth)
+    /* stars */
+    const StarTexture = new TextureLoader().load('static/textures/sky.jpg')
+    let StarGeometry = new SphereGeometry(1200, 10, 10)
+    let StarMaterial = new MeshBasicMaterial({ side: BackSide, map: StarTexture })
+    this.Star = new Mesh(StarGeometry, StarMaterial)
+    this.Star.position.x = 0
+    this.add(this.Star)
+    /* dataPoint */
+    for (let i = 0; i < 200; i++) {
+      let points = []
+      // sydney 33.8688° S, 151.2093° E
+      let lat = -33.8688 + (Math.random() * 30)
+      let lon = 151.2093 + (Math.random() * 30)
+      let sydney = calcPosition(lat, lon, EarthRadius)
+      let sydney2 = calcPosition(lat, lon, EarthRadius + 10 + (Math.random() * 50))
+      points.push(new Vector3(sydney[0], sydney[1], sydney[2]))
+      points.push(new Vector3(sydney2[0], sydney2[1], sydney2[2]))
+      let geometry = new BufferGeometry().setFromPoints(points)
+      let material = new LineBasicMaterial({color: 0xff00ff})
+      let data = new Line(geometry, material, 5)
+      this.add(data)
+    }
+  }
+  update () {
 
-    let geometry = new SphereGeometry(200, 50, 50)
-    let material = new MeshBasicMaterial({ color: 0x00ff00 })
-    let Earth = new Mesh(geometry, material)
-    Earth.position.x = 0
-    this.add(Earth)
   }
 }
 
+function radians (degrees) {
+  let radians = degrees * (Math.PI / 180)
+  return radians
+}
+
+function calcPosition (lat, lon, radius) {
+  let phi = radians(90 - lat)
+  let theta = radians(lon + 180)
+  let x = -((radius) * Math.sin(phi) * Math.cos(theta))
+  let z = ((radius) * Math.sin(phi) * Math.sin(theta))
+  let y = ((radius) * Math.cos(phi))
+  return [x, y, z]
+}
 export default GlobeScene
