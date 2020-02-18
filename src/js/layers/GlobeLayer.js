192bdc7c01b2969246a62b442d55c506ee0d428e
Luke Franzke <lukefranzke@gmail.com>
2020-02-18 15:08:33 +0100

    Basic mechanism for reading geotiff data

--- src/js/layers/GlobeLayer.js
+++ src/js/layers/GlobeLayer.js
@@ -9,16 +9,15 @@ import {
   Line,
   Vector3,
   BufferGeometry,
-  LineBasicMaterial
+  LineBasicMaterial,
+  UVMapping
 } from 'three'
 
 class GlobeLayer extends BaseLayer {
   setup () {
     super.setup()
-
     // disable zoom for globe
-    this.controls.enableZoom = false
-
+    this.controls.enablePan = false
     let EarthRadius = 200
     /* earth */
     const EarthTexture = new TextureLoader().load('static/textures/earthMono8k.jpg')
@@ -35,24 +34,41 @@ class GlobeLayer extends BaseLayer {
     this.Star.position.x = 0
     this.scene.add(this.Star)
     /* dataPoint */
-    for (let i = 0; i < 200; i++) {
+    // todo: need to correct for different map projection.
+    var texture
+    var crowtherData
+    var myScene = this.scene
+    texture = new TextureLoader().load('static/crowther_data/Bastin_19_Restoration_Potential_Mini.jpg', function (event) {
+      crowtherData = getImageData(texture.image)
+      drawData(myScene, crowtherData, texture.image.width, texture.image.height, EarthRadius)
+    })
+  }
+  update () {
+
+  }
+}
+
+function drawData (scene, crowtherData, width, height, EarthRadius) {
+  for (let x = 0; x < width; x++) {
+    let lon = x / width
+    lon -= 0.5
+    lon *= 360.0
+    console.log(lon)
+    for (let y = 0; y < height; y++) {
+      let red = getPixel(crowtherData, x, height - y).r
+      let lat = y / height
+      lat -= 0.5
+      lat *= 180.0
       let points = []
-      // sydney 33.8688° S, 151.2093° E
-      let lat = -33.8688 + (Math.random() * 30)
-      let lon = 151.2093 + (Math.random() * 30)
-      let sydney = calcPosition(lat, lon, EarthRadius)
-      let sydney2 = calcPosition(lat, lon, EarthRadius + 10 + (Math.random() * 50))
-      points.push(new Vector3(sydney[0], sydney[1], sydney[2]))
-      points.push(new Vector3(sydney2[0], sydney2[1], sydney2[2]))
+      let startLine = calcPosition(lat, lon, EarthRadius)
+      let endLine = calcPosition(lat, lon, EarthRadius + (red / 10 + 1))
+      points.push(new Vector3(startLine[0], startLine[1], startLine[2]))
+      points.push(new Vector3(endLine[0], endLine[1], endLine[2]))
       let geometry = new BufferGeometry().setFromPoints(points)
       let material = new LineBasicMaterial({ color: 0xff00ff })
       let data = new Line(geometry, material, 5)
-      this.scene.add(data)
+      scene.add(data)
     }
-  }
-
-  update () {
-
   }
 }
 
@@ -68,6 +84,21 @@ function calcPosition (lat, lon, radius) {
   let z = ((radius) * Math.sin(phi) * Math.sin(theta))
   let y = ((radius) * Math.cos(phi))
   return [x, y, z]
+}
+
+function getImageData (image) {
+  let canvas = document.createElement('canvas')
+  canvas.width = image.width
+  canvas.height = image.height
+  let context = canvas.getContext('2d')
+  context.drawImage(image, 0, 0)
+  return context.getImageData(0, 0, image.width, image.height)
+}
+
+function getPixel (imagedata, x, y) {
+  let position = (x + imagedata.width * y) * 4
+  let data = imagedata.data
+  return { r: data[ position ], g: data[ position + 1 ], b: data[ position + 2 ], a: data[ position + 3 ] }
 }
 
 export default GlobeLayer
